0e2d95e7d9ccd41379d6c344bc644bd63d4fd3a6
Reza Rahimlou <mr.rezarahimlou@gmail.com>
2024-10-22 08:35:46 +0330

    Add project files.

---
+++ Bookify.Application/Abstractions/Behaviors/ValidationBehavior.cs
@@ -0,0 +1,46 @@
+﻿using Bookify.Application.Abstractions.Messaging;
+using Bookify.Application.Exceptions;
+using FluentValidation;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bookify.Application.Abstractions.Behaviors
+{
+    public class ValidationBehavior<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse> where TRequest : IBaseCommand
+    {
+        private readonly IEnumerable<IValidator<TRequest>> _valdators;
+
+        public ValidationBehavior(IEnumerable<IValidator<TRequest>> valdators)
+        {
+            _valdators = valdators;
+        }
+
+        public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
+        {
+            if (!_valdators.Any())
+            {
+                return await next();
+            }
+
+            var context = new ValidationContext<TRequest>(request);
+
+            var validationErrors = _valdators
+                .Select(validator => validator.Validate(context))
+                .Where(validationResult => validationResult.Errors.Any())
+                .SelectMany(validationResult => validationResult.Errors)
+                .Select(validationFailure => new ValidationError(validationFailure.PropertyName, validationFailure.ErrorMessage))
+                .ToList();
+
+            if (validationErrors.Any())
+            {
+                throw new Exceptions.ValidationException(validationErrors);
+            }
+
+            return await next();
+        }
+    }
+}
---
+++ Bookify.Infrastructure/ApplicationDbContext.cs
@@ -0,0 +1,66 @@
+﻿using Bookify.Application.Exceptions;
+using Bookify.Domain.Abstractions;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bookify.Infrastructure
+{
+    public sealed class ApplicationDbContext : DbContext, IUnitOfWork
+    {
+        private readonly IPublisher _publisher;
+        public ApplicationDbContext(DbContextOptions options, IPublisher publisher) : base(options)
+        {
+            _publisher = publisher;
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext).Assembly);
+
+            base.OnModelCreating(modelBuilder);
+        }
+
+        public override async Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+        {
+            try
+            {
+                var result = await base.SaveChangesAsync(cancellationToken);
+                if (result > 0)
+                {
+                    await PublishDomainEventAsync();
+                }
+
+                return result;
+            }
+            catch (DbUpdateConcurrencyException ex)
+            {
+                throw new ConcurrencyException("Concurrency Exception occured.", ex);
+            }
+            
+        }
+
+        private async Task PublishDomainEventAsync()
+        {
+            var domainEvents = ChangeTracker
+                .Entries<Entity>()
+                .Select(entry => entry.Entity)
+                .SelectMany(entity =>
+                {
+                    var domainEvnets = entity.GetDomainEvents();
+                    entity.ClearDomainEvents();
+                    return domainEvnets;
+                })
+                .ToList();
+
+            foreach (var domainEvent in domainEvents)
+            {
+                await _publisher.Publish(domainEvent);
+            }
+        }
+    }
+}
